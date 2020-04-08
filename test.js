f498337e1f916eb02bba0929f04246bd4fdf0d3a
marcinstenka <44028611+marcinstenka@users.noreply.github.com>
2020-04-08 10:45:45 +0200

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const tile = document.querySelector('.tile6');
-const nauka = document.querySelector('.additionalTile');
-
-tile.addEventListener("mouseover", () => {
-    nauka.style.display = 'grid'
-})
-tile.addEventListener("mouseout", () => {
-    nauka.style.display = 'none'
-})
